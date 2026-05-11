367a483f9f65a562d788d70aca52fbd50b079d7e
Minato-Dev-Hokage <minatodev776@gmail.com>
2026-05-11 21:13:25 +0100

    Create system/setting.js

---
+++ system/setting.js
@@ -0,0 +1,28 @@
+
+global.prefix = [
+  ".", "!", "🐤", "🗿", "#", "/", "\\", "~", "&", 
+  "*", "+", "?", ">", "<", ":", ";", "|", "^", "%", 
+  "$", "€", "£", "¥", "=", "-", "_", "@", "©", "®", 
+  "™", "✓", "✔", "•", "×", "¶", "¤", "§", "∞"
+];
+global.public = true
+global.linkOwner = "https://wa.me/242068906671"
+global.linkch = "https://whatsapp.com/channel/0029VbAj0uCLikg6Pfjs4i2u"
+global.owner = ["242068906671"] 
+global.nameowner = "</> 𝙻𝙾𝚁𝙳 𝙼𝙸𝙽𝙰𝚃𝙾 𝙳𝙴𝚅" 
+global.author = ["242068906671"] 
+global.nameauthor = "MOTHER FUCKER" 
+global.namebot = "Bot name" 
+global.bot = {
+  newsletterJid: 
+    "120363419855570475@newsletter",
+};
+
+//======================
+global.mess = { 
+author: '*ʏᴏᴜ ᴀʀᴇ ɴᴏᴛ ᴀɴ ᴀᴜᴛʜᴏʀɪᴢᴇᴅ ᴜsᴇʀ* ✘', 
+owner: '*ʏᴏᴜ ᴀʀᴇ ɴᴏᴛ ᴛʜᴇ ᴏᴡɴᴇʀ* ✘',
+premium: '*ʏᴏᴜ ᴀʀᴇ ɴᴏᴛ ᴀ ᴘʀᴇᴍɪᴜᴍ ᴜsᴇʀ* ✘',
+succes: '*sᴜᴄᴄᴇs ☑*︎'
+}
+//======================
